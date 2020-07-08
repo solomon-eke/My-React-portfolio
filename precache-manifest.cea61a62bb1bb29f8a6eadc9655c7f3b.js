ec249aab779d9bbd5bd2ada28273e88d2d86a816
Solomon <ekesolomon11@gmail.com>
2020-07-07 20:06:55 -0400

    Updates

--- precache-manifest.cea61a62bb1bb29f8a6eadc9655c7f3b.js
+++ precache-manifest.cea61a62bb1bb29f8a6eadc9655c7f3b.js
@@ -1,26 +1,26 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "ae2b0e7fef0eddf6fb4927636947053f",
-    "url": "/User-Directory/index.html"
+    revision: 'ae2b0e7fef0eddf6fb4927636947053f',
+    url: '/My-React-portfolio/index.html'
   },
   {
-    "revision": "3cc6843fcacaeccb106a",
-    "url": "/User-Directory/static/css/main.268ef553.chunk.css"
+    revision: '3cc6843fcacaeccb106a',
+    url: '/My-React-portfolio/static/css/main.268ef553.chunk.css'
   },
   {
-    "revision": "29a6451428f7846114fc",
-    "url": "/User-Directory/static/js/2.4e540142.chunk.js"
+    revision: '29a6451428f7846114fc',
+    url: '/My-React-portfolio/static/js/2.4e540142.chunk.js'
   },
   {
-    "revision": "0a572ad323474bafc6a871b6336141bd",
-    "url": "/User-Directory/static/js/2.4e540142.chunk.js.LICENSE.txt"
+    revision: '0a572ad323474bafc6a871b6336141bd',
+    url: '/My-React-portfolio/static/js/2.4e540142.chunk.js.LICENSE.txt'
   },
   {
-    "revision": "3cc6843fcacaeccb106a",
-    "url": "/User-Directory/static/js/main.6b362b95.chunk.js"
+    revision: '3cc6843fcacaeccb106a',
+    url: '/My-React-portfolio/static/js/main.6b362b95.chunk.js'
   },
   {
-    "revision": "e1c83858e31256f2d3c4",
-    "url": "/User-Directory/static/js/runtime-main.941ed641.js"
+    revision: 'e1c83858e31256f2d3c4',
+    url: '/My-React-portfolio/static/js/runtime-main.941ed641.js'
   }
-]);
+])
--- service-worker.js
+++ service-worker.js
@@ -11,29 +11,33 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts(
+  'https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js'
+)
 
 importScripts(
-  "/User-Directory/precache-manifest.cea61a62bb1bb29f8a6eadc9655c7f3b.js"
-);
+  '/My-React-portfolio/precache-manifest.cea61a62bb1bb29f8a6eadc9655c7f3b.js'
+)
 
-self.addEventListener('message', (event) => {
+self.addEventListener('message', event => {
   if (event.data && event.data.type === 'SKIP_WAITING') {
-    self.skipWaiting();
+    self.skipWaiting()
   }
-});
+})
 
-workbox.core.clientsClaim();
+workbox.core.clientsClaim()
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
  * requests for URLs in the manifest.
  * See https://goo.gl/S9QRab
  */
-self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+self.__precacheManifest = [].concat(self.__precacheManifest || [])
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {})
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/User-Directory/index.html"), {
-  
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
-});
+workbox.routing.registerNavigationRoute(
+  workbox.precaching.getCacheKeyForURL('/My-React-portfolio/index.html'),
+  {
+    blacklist: [/^\/_/, /\/[^/?]+\.[^/]+$/]
+  }
+)
